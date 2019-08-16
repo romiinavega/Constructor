4fe4d865fe5d75fed796983c4e030d4ff6f61df7
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-16 08:46:48 -0700

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Contacto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Contacto
+    {
+        public string Nombre { get; set; }
+        public string Celular { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+
+        public Contacto ()
+        {
+            Nombre = "Sin nombre";
+            Celular = "Sin celular";
+            Telefono = "Sin telefono";
+            Correo = "Sin correo";
+            Direccion = "Sin direccion";
+        }
+
+        public Contacto(string nombre, string correo)
+        {
+            Nombre = nombre;
+            Correo = correo;
+            Celular = "Sin ceular";
+            Telefono = "Sin telefono";
+            Direccion = "Sin direccion";
+
+
+        }
+        public Contacto(string nombre, string celular, string telefono, string correo, string direccion)
+        {
+            Nombre = nombre;
+            Celular = celular;
+            Telefono = telefono;
+            Correo = correo;
+            Direccion = direccion;
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contacto contacto1 = new Contacto();
+            contacto1.Nombre = "Jose";
+            contacto1.Celular = "(644) 4 00 00 00";
+            contacto1.Correo = "jose@correo.com";
+
+            Console.WriteLine("Nombre: " + contacto1.Nombre);
+            Console.WriteLine("Correo: " + contacto1.Correo);
+            Console.WriteLine("Direccion: " + contacto1.Direccion);
+
+            Contacto contacto2 = new Contacto("Juan", "juan@correo.com");
+            Console.WriteLine("*Contacto 2:");
+            Console.WriteLine("Nombre: " + contacto2.Nombre);
+            Console.WriteLine("Correo: " + contacto2.Correo);
+            Console.WriteLine("Direccion: " + contacto2.Direccion);
+            Console.ReadLine();
+
+
+        }
+    }
+}
